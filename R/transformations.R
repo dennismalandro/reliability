422414a1ad0574707bd686c4088bd43259b40e0e
Dennis Malandro <dennismalandro@gmail.com>
2023-05-24 11:45:51 -0400

    Initial commit

---
+++ R/transformations.R
@@ -0,0 +1,25 @@
+#' Probability-model linearizing transformations
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' weibull_trans()
+weibull_trans <- function() {
+  scales::trans_new('weibull',
+    function(x) log(-log(1 - x)),
+    function(x) 1 - exp(-exp(x)))}
+
+frechet_trans <- function() {
+  scales::trans_new('weibull',
+    function(x) -log(-log(x)),
+    function(x) exp(-exp(-x)))}
+
+arrhenius_trans <- function() {
+  scales::trans_new(
+    name = 'arrhenius',
+    transform = function(x) 1 / (x + 273.15),
+    inverse = function(x) 1 / x - 273.15,
+    domain = c(-273.15, Inf))}
+
+arrh <- function(temp_c) 1 / (temp_c + 273.15)
